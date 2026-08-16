7b50a32271c5f67a93933dc5a1cc0854f29c312f
ZhuLz02 <200554155+ZhuLz02@users.noreply.github.com>
2026-08-17 00:30:04 +0800

    时钟修复

--- images/axin/renxi/functions.js
+++ images/axin/renxi/functions.js
@@ -38,8 +38,9 @@ $(window).resize(function() {
 })(jQuery);
 
 function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var current = new Date();
+	var seconds = Math.floor((current.getTime() - date.getTime()) / 1000);
+	if (seconds < 0) seconds = 0;
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
